5d40d03a7c23d9d2da307655d639efe5c95337f5
filipeprime <filipeam@primeinformatica.com.br>
2018-09-18 14:57:14 -0300

    Primeira configuração

---
+++ src/main/java/br/com/primeinf/escala_sustentacao/EscalaSustentacaoApplication.java
@@ -0,0 +1,12 @@
+package br.com.primeinf.escala_sustentacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalaSustentacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalaSustentacaoApplication.class, args);
+	}
+}
